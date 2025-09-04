87a263b427768463e613ed0c528a698d0e658ce8
Randall Tetteh-Kumah <135046789+Randall-T@users.noreply.github.com>
2025-09-04 10:25:23 +0000

    Added link for render backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,6 +14,7 @@ import "./App.css";
 
 // Configure axios defaults
 axios.defaults.baseURL = "http://localhost:5000/api";
+("https://pharmacy-system-backend.onrender.com/api");
 
 const App = () => {
   const [user, setUser] = useState(null);
